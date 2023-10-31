b2dc0cca0468cbf833787b857750e2ea4210b6dd
Paul McCowat <paul@mccowat.com>
2023-10-31 15:47:55 +0000

    ossec

--- install.bat
+++ install.bat
@@ -14,6 +14,10 @@ set SYSMON_DIR=C:\ProgramData\sysmon
 set SYSMON_CONF=sysmonconfig.xml
 set SYSMON_BIN=C:\Source\sysmon
 
+if not exist %SYSMON_BIN% (
+	mkdir %SYSMON_BIN%
+	)
+
 echo.
 echo [+] Checking powershell version...
 @powershell if ($PSVersionTable.PSVersion.Major -ge 5) { Write-Host " [+] You are running Powershell version $PSVersionTable.PSVersion.Major"} else { Write-Host " [-] Powershell version $PSVersionTable.PSVersion.Major detected, please update to version 5 or above."; exit(1) }
@@ -37,34 +41,49 @@ echo ####################SYSMON####################
 :: download sysmon
 @powershell Invoke-WebRequest -Uri "https://live.sysinternals.com/Sysmon64.exe" -OutFile "C:\Source\Sysmon\Sysmon64.exe"
 @powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Neo23x0/sysmon-config/master/sysmonconfig-export-block.xml" -OutFile "C:\Source\Sysmon\sysmonconfig.xml"
+@powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pmccowat/sysmon-auto-install/master/auto_update.bat" -OutFile "C:\Source\Sysmon\auto_update.bat"
 
+xcopy %SYSMON_BIN%\sysmon64.exe %SYSMON_DIR% /q /y
+xcopy %SYSMON_BIN%\%SYSMON_CONF% %SYSMON_DIR% /q /y
+xcopy %SYSMON_BIN%\auto_update.bat %SYSMON_DIR% /q /y
+
+schtasks /delete /TN "Update_Sysmon_Rules" /F
+
+if not exist %SYSMON_DIR% (
+	mkdir %SYSMON_DIR%
+	)
 
 sc query sysmon >nul
 if "%errorlevel%" EQU "0" (
 	echo.
 	echo [+] Sysmon installed, removing...
-	sysmon.exe -nobanner -u force
+	sysmon.exe -u
 	)
 
 
 sc query sysmon64 >nul
 if "%errorlevel%" EQU "0" (
 	echo.
-	echo [+] Sysmon installed, removing...
-	sysmon64.exe -nobanner -u force
+	echo [+] Sysmon64 installed, removing...
+	sysmon64.exe -u force
+	)
+
+sc query sysmon64 >nul
+if "%errorlevel%" EQU "0" (
+	echo.
+	echo [+] Sysmon64 installed, removing...
+	sysmon64.exe -u
 	)
 
 echo.
 echo [+] Copying sysmon and config...
-if not exist %SYSMON_DIR% (
-	mkdir %SYSMON_DIR%
-	)
+
 pushd %SYSMON_DIR%
-xcopy /q/y %SYSMON_BIN%\sysmon64.exe %SYSMON_DIR%
-xcopy /q/y %SYSMON_BIN%\%SYSMON_CONF% %SYSMON_DIR%
+
+
 echo [+] Installing sysmon and applying config...
-sysmon64.exe -nobanner -accepteula -i %SYSMON_CONF%
-sc failure Sysmon64 actions= restart/10000/restart/10000// reset= 120
+sysmon64.exe -i -accepteula
+::sc failure Sysmon64 actions= restart/10000/restart/10000// reset= 120
 echo.
 echo [+] Creating daily update task
 :: add scheduler task to update sysmon config with start time based on when the task is added
@@ -82,16 +101,13 @@ if "%hour:~1,1%"=="" set hour=0%hour%
 if "%minute:~1,1%"=="" set minute=0%minute%
 set tasktime=%hour%:%minute%
 ::
-xcopy /q/y %SCRIPT_DIR%auto_update.bat %SYSMON_DIR%
-xcopy /q/y %SCRIPT_DIR%Sigcheck64.exe %SYSMON_DIR%
-SchTasks /create /tn "Update sysmon and winlogbeat config" /ru SYSTEM /rl HIGHEST /sc daily /tr "cmd.exe /c \"%SYSMON_DIR%\\auto_update.bat\"" /f /st %tasktime%
-powershell Get-Service sysmon64 || goto :service_error
 
+SchTasks /create /tn "Update sysmon" /ru SYSTEM /rl HIGHEST /sc daily /tr "cmd.exe /c \"%SYSMON_DIR%\\auto_update.bat\"" /f /st %tasktime%
 
 echo ####################PS LOGGING####################
 
 echo.
-echo [+] Importing PS logging registries and applying config...
+::echo [+] Importing PS logging registries and applying config...
 :: https://www.malwarearchaeology.com/logging. 
 :: These settings will only change the local security policy.  It is best to set these in Group Policy default profile so all systems get the same settings.  
 :: GPO will overwrite these settings!
@@ -121,7 +137,7 @@ wevtutil sl "Windows Powershell" /ms:256000100
 ::
 wevtutil sl "Microsoft-Windows-PowerShell/Operational" /ms:256000100
 ::
-wevtutil sl "Microsoft-Windows-Sysmon/Operational" /ms:256000100
+::wevtutil sl "Microsoft-Windows-Sysmon/Operational" /ms:256000100
 ::
 ::#######################################################################
 ::
@@ -130,34 +146,6 @@ wevtutil sl "Microsoft-Windows-Sysmon/Operational" /ms:256000100
 :: ---------------------------------------------------------------------
 ::
 wevtutil sl "Microsoft-Windows-TaskScheduler/Operational" /e:true
-::
-::#######################################################################
-::
-:: Creates profile.ps1 in the correct location - SET Command variables for PowerShell - Enables default profile to collect PowerShell Command Line parameters and allows .PS1 to execute
-:: --------------------------------------------------------------------------------------------------------------------------
-::
-:: Allows local powershell scripts to run
-::
-powershell Set-ExecutionPolicy RemoteSigned
-::
-:: For powershell version 2-4. Not adding to profile.ps1 because it will output the registry every time powershell is opened. -ahuang
-::echo Get-Item "hklm:\software\microsoft\windows\currentversion\policies\system\audit" > c:\windows\system32\WindowsPowerShell\v1.0\profile.ps1
-findstr /i "LogCommandHealthEvent" c:\windows\system32\WindowsPowerShell\v1.0\profile.ps1 >nul
-if "%errorlevel%" NEQ "0" (
-	echo $LogCommandHealthEvent = $true >> c:\windows\system32\WindowsPowerShell\v1.0\profile.ps1
-	)
-findstr /i "LogCommandLifecycleEvent" c:\windows\system32\WindowsPowerShell\v1.0\profile.ps1 >nul
-if "%errorlevel%" NEQ "0" (
-	echo $LogCommandLifecycleEvent = $true >> c:\windows\system32\WindowsPowerShell\v1.0\profile.ps1
-	)
-::
-:: importing ps_logging.reg to enable powershell logging.
-reg import "%SCRIPT_DIR%ps_logging.reg" >nul
-
-:: Disable audit category
-:: May need to disable these if logs get too noisy
-:: 
-::auditpol /set /subcategory:"Filtering Platform Connection" /success:disable /failure:disable
 
 echo.
 echo [+] Script finished.
@@ -168,4 +156,11 @@ goto end
 echo [-] Service failed to start. Script will exit.
 
 :end
-pause >nul
+ping localhost
+
+@powershell Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.5.4-1.msi -OutFile wazuh-agent.msi; ./wazuh-agent.msi /q WAZUH_MANAGER='siem.lochsandglens.com' WAZUH_REGISTRATION_SERVER='siem.lochsandglens.com' WAZUH_PROTOCOL='TCP' WAZUH_AGENT_GROUP='default' 
+ping localhost
+@powershell Invoke-WebRequest -Uri "https://live.sysinternals.com/Sysmon64.exe" -OutFile "C:\Source\Sysmon\Sysmon64.exe"
+ping localhost
+
+net start Wazuh
